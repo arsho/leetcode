25a095550b24deec02c5cf2008500e2302c03a87
arsho <shovon.sylhet@gmail.com>
2018-03-07 18:09:07 +0600

    updated utils

--- utils/add_new_problem.py
+++ utils/add_new_problem.py
@@ -9,17 +9,18 @@ def update_readme(title, filename_title, url, category):
     with open("../README.md", "a") as readme:
         readme.write("| [{}]({}) | {} | [Solution](solutions/{}) |\n".format(title, url, category, filename_title))
 
-
 def make_problem_directory(repo_path, filename_title):
     directory = os.path.join(repo_path,"solutions",filename_title)
     if not os.path.exists(directory):
         os.makedirs(directory)
+    return str(directory)
 
-title = "121. Best Time to Buy and Sell Stock"#input("Add new problem title: ")
+title = input("Add new problem title: ")
 filename_title = get_filename_title(title)
-url = "https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/"#input("Add new problem url: ")
-category = "Dynamic Programming"#input("Problem Type: ")
+url = input("Add new problem url: ")
+category = input("Problem Type: ")
 current_path = os.path.dirname(os.path.realpath(__file__))
 repo_path = os.path.abspath(os.path.join(current_path, os.pardir))
 update_readme(title, filename_title, url, category)
-make_problem_directory(repo_path, filename_title)
+directory = make_problem_directory(repo_path, filename_title)
+print("Done. Open {}".format(directory))
