c3ecb1d99e3f2d5612e7cfbbafa908dbce2be2a4
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2019-12-22 12:15:00 +0600

    5291

---
+++ solutions/5291_Find_Numbers_with_Even_Number_of_Digits/solution_arsho.py
@@ -0,0 +1,12 @@
+"""
+Title     : 5291. Find Numbers with Even Number of Digits
+Category  : Array
+URL       : https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+Author    : arsho
+Created   : 22 December 2019
+"""
+from typing import List
+
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        return len([i for i in nums if len(str(i)) % 2 == 0])
