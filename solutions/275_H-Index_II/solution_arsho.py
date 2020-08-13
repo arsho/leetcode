d652de48be86f7153d2c681c77bb59bc3200711f
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2020-08-14 02:41:19 +0600

    updated 275

--- solutions/275_H-Index_II/solution_arsho.py
+++ solutions/275_H-Index_II/solution_arsho.py
@@ -7,7 +7,30 @@ Created   : 14 August 2020
 """
 from typing import List
 
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        total_papers = len(citations)
+        for i in range(total_papers, -1, -1):
+            left = 0
+            right = total_papers - 1
+            while left <= right:
+                mid = (left + right)//2
+                if citations[mid] >= i:
+                    right = mid-1
+                elif citations[mid] < i:
+                    left = mid+1
+            if total_papers - left >= i:
+                return i
+        return 0
 
+if __name__ == "__main__":
+    solution = Solution()
+    h_index = solution.hIndex([0,1,3,5,6])
+    assert h_index == 3
+
+
+"""
+Alternative solution
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         for i in range(len(citations), -1, -1):
@@ -19,3 +42,5 @@ class Solution:
             if count >= i:
                 return i
         return 0
+
+"""
