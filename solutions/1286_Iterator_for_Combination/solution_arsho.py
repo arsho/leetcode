031767ec2571f1697cfae34cf894657633fe2e82
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2020-08-13 23:36:45 +0600

    added updated solution for 1286

--- solutions/1286_Iterator_for_Combination/solution_arsho.py
+++ solutions/1286_Iterator_for_Combination/solution_arsho.py
@@ -5,6 +5,43 @@ URL       : https://leetcode.com/problems/iterator-for-combination/
 Author    : arsho
 Created   : 13 August 2020
 """
+
+
+class CombinationIterator:
+
+    def __init__(self, characters: str, combination_length: int):
+        self.characters = characters
+        self.combinations = self.get_combinations(characters,
+                                                  combination_length)
+        self.total_combinations = len(self.combinations)
+        self.iterator_position = 0
+
+    def next(self) -> str:
+        word = self.combinations[self.iterator_position]
+        self.iterator_position += 1
+        return word
+
+    def hasNext(self) -> bool:
+        return self.iterator_position < self.total_combinations
+
+    def get_combinations(self, characters, valid_length) -> list:
+        combinations = []
+        total_characters = len(characters)
+        max_bit = (2 ** total_characters) - 1
+        for i in range(max_bit, -1, -1):
+            binary_presentation = str(bin(i))[2:].zfill(total_characters)
+            if binary_presentation.count("1") == valid_length:
+                word = ""
+                for j in range(total_characters):
+                    if binary_presentation[j] == "1":
+                        word += characters[j]
+                combinations.append(word)
+        return combinations
+
+"""
+Using combinations method from itertools package
+"""
+"""
 from itertools import combinations
 
 
@@ -23,8 +60,4 @@ class CombinationIterator:
 
     def hasNext(self) -> bool:
         return self.iterator_position < self.combinations_length
-
-# Your CombinationIterator object will be instantiated and called as such:
-# obj = CombinationIterator(characters, combinationLength)
-# param_1 = obj.next()
-# param_2 = obj.hasNext()
+"""
