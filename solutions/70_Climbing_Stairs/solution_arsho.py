e593f1b75470bb6284c27776af5070e5e359badb
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2020-08-15 12:25:25 +0600

    modified 70

--- solutions/70_Climbing_Stairs/solution_arsho.py
+++ solutions/70_Climbing_Stairs/solution_arsho.py
@@ -1,13 +1,8 @@
-class Solution(object):
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        dp = [1 for i in range(n+1)]
-        if n>1:
-            dp[0], dp[1], dp[2] = 1, 1, 2
-        for i in range(3, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n]
-        
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        res = []
+        res.append(1)
+        res.append(1)
+        for i in range(n + 1):
+            res.append(res[-1] + res[-2])
+        return res[n]
