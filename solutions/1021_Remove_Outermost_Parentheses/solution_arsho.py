ada9d5c285f6ee480e8aa5229ef285f36163be18
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2021-03-30 19:49:06 +0600

    added 1021 python

--- solutions/1021_Remove_Outermost_Parentheses/solution_arsho.py
+++ solutions/1021_Remove_Outermost_Parentheses/solution_arsho.py
@@ -5,3 +5,20 @@ URL       : https://leetcode.com/problems/remove-outermost-parentheses/
 Author    : arsho
 Created   : 30 March 2021
 """
+
+
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        output = ""
+        b = 0
+        stack = []
+        for i, c in enumerate(s):
+            if c == "(":
+                stack.append(c)
+            else:
+                stack.pop(-1)
+            if i != 0:
+                if len(stack) == 0:
+                    output += "".join(s[b + 1:i])
+                    b = i + 1
+        return output
